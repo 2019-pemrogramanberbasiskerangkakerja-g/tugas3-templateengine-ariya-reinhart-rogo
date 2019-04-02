7c5293c30e5c7a6f052b4cba653d87526cbacacf
rogojagad <rogojagadalit@gmail.com>
2019-04-02 12:16:06 +0700

    author data

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('pages/index', {title: "Yeet-Site"})
+    res.render('pages/index', { title: "Yeet-Site" })
 })
 
 app.get('/about', (req, res) => {
-    res.render('pages/about', {title: "Yeet-Site | About"})
+    let authorData = [
+        {
+            nama: "Ariya Wildan Devanto",
+            nrp: "05111540000123"
+        },
+        {
+            nama: "Reinhart Ceisar Butar Butar",
+            nrp: "05111540000132"
+        },
+        {
+            nama: "Rogo Jagad Alit",
+            nrp: "05111540000168"
+        },
+    ]
+
+    res.render('pages/about', {
+        title: "Yeet-Site | About",
+        authorData: authorData,
+        authorCount: authorData.length
+    })
 })
 
 app.listen(3000, () => {
